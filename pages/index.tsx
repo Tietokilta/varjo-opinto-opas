cb5dee1861666136d13061cabeab6a625e1a8b5d
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-08-27 15:29:48 +0300

    Add basic Eslint + Prettier configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
-      <h1 className='font-bold text-6xl text-white'>Welcome to Varjo-opinto-opas</h1>
+    <div className="flex h-screen items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-6xl font-bold text-white">
+        Welcome to Varjo-opinto-opas
+      </h1>
     </div>
   )
 }
