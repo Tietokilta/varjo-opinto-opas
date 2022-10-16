c907ad52bf31907ea58abe73dc68973ee039abd0
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-10-16 18:48:59 +0300

    Add header link to front page

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header className="flex items-center justify-between rounded-full bg-blue-900 py-2 px-6">
-      <h1 className="text-2xl font-bold text-neutral-50">VOO</h1>
+      <Link href="/">
+        <h1 className="text-2xl font-bold text-neutral-50 hover:cursor-pointer">
+          VOO
+        </h1>
+      </Link>
       {/* TODO: Make language selector functional */}
       <button className="text-md text-xs text-blue-300">English</button>
     </header>
