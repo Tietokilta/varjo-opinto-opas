4b09b34067e76f63f2e4394f255b0269b476de72
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-10-16 18:35:06 +0300

    Add link to result rows

--- packages/frontend/components/ResultsViewer/ResultTable/ResultRow.tsx
+++ packages/frontend/components/ResultsViewer/ResultTable/ResultRow.tsx
@@ -1,8 +1,13 @@
 import { courseBasics } from '../../../types/types'
+import { useRouter } from 'next/router'
 
 const ResultRow = ({ result }: { result: courseBasics }) => {
+  const router = useRouter()
   return (
-    <tr className="border-b transition-all hover:scale-[1.03] hover:bg-white hover:shadow-lg">
+    <tr
+      className="border-b transition-all hover:scale-[1.03] hover:bg-white hover:shadow-lg"
+      onClick={() => router.push('/course/' + result.code)}
+    >
       <th className="py-2 px-4 text-left">{result.name}</th>
       <td className="py-2 px-4">{result.code}</td>
       <td className="py-2 px-4 text-center">{result.period}</td>
