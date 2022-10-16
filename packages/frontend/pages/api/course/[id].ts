6afc684fb92c5487cd0506316deb1398bc9ad5d3
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-10-16 13:20:26 +0300

    Offer mockdata from coursedata endpoint

--- packages/frontend/pages/api/course/[id].ts
+++ packages/frontend/pages/api/course/[id].ts
@@ -1,5 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const mockCourseData = {
+  name: 'Tuotantotalous 1',
+  code: 'TU-001',
+  period: '3-5',
+  credits: '5',
+  rating: '4.9',
+  workload: '3.9',
+  description:
+    'Kurssin suoritettuaan opiskelijalla on paremmat valmiudet ymmärtää ja arvioida liiketoimintamahdollisuuksia, suunnitella ja arvioida liiketoimintaprosesseja ja analysoida yrityksen taloudellista tilaa. Tavoitteena on myös oppia yleisiä insinöörityön valmiuksia projektin suunnitteluun, organisointiin ja hallintaan',
+  learningGoals:
+    'Kurssin suoritettuaan opiskelijalla on paremmat valmiudet ymmärtää ja arvioida liiketoimintamahdollisuuksia, suunnitella ja arvioida liiketoimintaprosesseja ja analysoida yrityksen taloudellista tilaa. Tavoitteena on myös oppia yleisiä insinöörityön valmiuksia projektin suunnitteluun, organisointiin ja hallintaan',
+  reviews: [
+    { text: 'Ihan hyvä kurssi', rating: '4', workload: '3' },
+    { text: 'En tykännyt kyllä ollenkaan', rating: '2', workload: '4' },
+    { text: 'En sano juuta enkä jaata', rating: '3', workload: '3' },
+  ],
+}
+
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
@@ -7,7 +25,6 @@ export default function handler(
   const id = request.query.id
 
   if (request.method === 'GET') {
-    console.log('Would return courseFull info: ', id)
-    response.send('Would return full info for course ' + id)
+    response.status(200).json({ mockCourseData })
   }
 }
