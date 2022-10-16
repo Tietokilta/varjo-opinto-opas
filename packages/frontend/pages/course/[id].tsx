bd62c47ec72b324927bbcca86d1d480c5c4ba83c
Sakari Palojoki <palojoki.sakari@gmail.com>
2022-10-16 14:48:06 +0300

    Create coursepage and basic layout for it

---
+++ packages/frontend/pages/course/[id].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const CoursePage: NextPage = () => {
+  return (
+    <div className="template-columns grid-columns-2 container mx-auto grid-rows-4">
+      Hello!
+    </div>
+  )
+}
+
+export default CoursePage
